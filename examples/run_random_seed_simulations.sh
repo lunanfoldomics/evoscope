a0ee514345c3980565271cc8a19f8fdfe633a398
Lunan Foldomics LLC <lucazammataro@lunanfoldomicsllc.com>
2026-06-28 13:28:34 -0500

    Update run_random_seed_simulations.sh

--- examples/run_random_seed_simulations.sh
+++ examples/run_random_seed_simulations.sh
@@ -1,1 +1,11 @@
+evoscope --width 60 --height 40 --seed 38 --epochs 120 --initial_cells 30 --nutrient 6.9 --outdir runs/seed_38
+evoscope --width 60 --height 40 --seed 40 --epochs 120 --initial_cells 30 --nutrient 6.9 --outdir runs/seed_40
+evoscope --width 60 --height 40 --seed 53 --epochs 120 --initial_cells 30 --nutrient 6.9 --outdir runs/seed_53
+evoscope --width 60 --height 40 --seed 65 --epochs 120 --initial_cells 30 --nutrient 6.9 --outdir runs/seed_65
+evoscope --width 60 --height 40 --seed 89 --epochs 120 --initial_cells 30 --nutrient 6.9 --outdir runs/seed_89
+evoscope --width 60 --height 40 --seed 90 --epochs 120 --initial_cells 30 --nutrient 6.9 --outdir runs/seed_90
+evoscope --width 60 --height 40 --seed 96 --epochs 120 --initial_cells 30 --nutrient 6.9 --outdir runs/seed_96
+evoscope --width 60 --height 40 --seed 101 --epochs 120 --initial_cells 30 --nutrient 6.9 --outdir runs/seed_101
+evoscope --width 60 --height 40 --seed 104 --epochs 120 --initial_cells 30 --nutrient 6.9 --outdir runs/seed_104
+evoscope --width 60 --height 40 --seed 107 --epochs 120 --initial_cells 30 --nutrient 6.9 --outdir runs/seed_107
 
